004d6ea20c58a593632901001f387ecab7c6811b
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2024-06-24 19:44:39 -0300

    Bugfix in flautim/tasks.py

--- flautim/tasks.py
+++ flautim/tasks.py
@@ -10,12 +10,13 @@ def exec(command):
 @shared_task()
 def runExperiment_task():
     #os.system("bash ./runExperiment.sh")
+    command = "./runExperiment.sh"
     log("{} started".format(command))
     try:
-        output = exec("./runExperiment.sh")
-        log("{} finished: {}".format(command, output)
+        output = exec(command)
+        log("{} finished: {}".format(command, output))
     except Exception as ex:
-        log("{} finished with error: {}".format(command, repr(ex))
+        log("{} finished with error: {}".format(command, repr(ex)))
 
 @shared_task()
 def statusExperiment_task():
