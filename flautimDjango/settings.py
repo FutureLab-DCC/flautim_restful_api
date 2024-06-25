41afd29df9ceb458ec7caadba020a6f1ec0ce4ca
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2024-06-25 09:23:21 -0300

    Update Allowed Hosts in settings.py

--- flautimDjango/settings.py
+++ flautimDjango/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)ku@kyi$=t1+#^19bn@15id33=!6ns#-u^o$+#qf4u)r%+d$bu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.7.77','localhost','127.0.0.1']
 
 
 # Application definition
