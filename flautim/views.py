1017a29bd3e34ec71147301a81a81bbb85a2bb9b
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2024-06-25 17:28:13 -0300

    Json response

--- flautim/views.py
+++ flautim/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework.renderers import JSONRenderer, renderer_classes
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from django.http import HttpResponse
 from .models import log, logs_collection
