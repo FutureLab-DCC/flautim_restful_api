caffa94f2079b80a6b9473fa13329222a9d0718d
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2024-06-25 08:01:50 -0300

    Improvements on logging and exception handling

--- flautim/views.py
+++ flautim/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import logs_collection
+from .models import log, logs_collection
 import os
 import subprocess
 from flautim.tasks import runExperiment_task, stopExperiment_task, statusExperiment_task, deleteExperiment_task
@@ -16,37 +16,45 @@ def index(request):
     return render(request, 'index.html', context) 
 
 def runExperiment(request):
-    records = { "call" : "experiment/run/" }
+    log("Service call experiment/run/")
     try:
         runExperiment_task.delay()
-        records["status"] = "ok"
+        records = { "status": "ok" }
     except Exception as ex:
-        records["status"] = "failed"
-        records["description"] = repr(ex)
+        records = { "status": "ok" }
+        log("experiment/run/ failed: {}".format(repr(ex)))
 
-    logs_collection.insert_one(records)
     return render(request, "run.html", records)
 
 def stopExperiment(request):
-    stopExperiment_task.delay()
-    records = {
-        "Call" : "Stop"
-    }
-    logs_collection.insert_one(records)
+    log("Service call experiment/stop/")
+    try:
+        stopExperiment_task.delay()
+        records = { "status": "ok" }
+    except Exception as ex:
+        records = { "status": "ok" }
+        log("experiment/stop/ failed: {}".format(repr(ex)))
+
     return render(request, "run.html", records)
     
 def statusExperiment(request):
-    statusExperiment_task.delay()
-    records = {
-        "Call" : "Status"
-    }
-    logs_collection.insert_one(records)
+    log("Service call experiment/status/")
+    try:
+        statusExperiment_task.delay()
+        records = { "status": "ok" }
+    except Exception as ex:
+        records = { "status": "ok" }
+        log("experiment/status/ failed: {}".format(repr(ex)))
+
     return render(request, "run.html", records)
 
 def deleteExperiment(request):
-    deleteExperiment_task.delay()
-    records = {
-        "Call" : "Delete"
-    }
-    logs_collection.insert_one(records)
+    log("Service call experiment/delete/")
+    try:
+        deleteExperiment_task.delay()
+        records = { "status": "ok" }
+    except Exception as ex:
+        records = { "status": "ok" }
+        log("experiment/delete/ failed: {}".format(repr(ex)))
+
     return render(request, "run.html", records)
