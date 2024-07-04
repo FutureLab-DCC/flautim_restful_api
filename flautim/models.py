3b15d370771269d384ac26cd0124837bea0cf168
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2024-07-04 20:08:09 -0300

    Rolling back MongoDB _id ObjectId

--- flautim/models.py
+++ flautim/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from utils import get_db_handle
 from datetime import datetime
-from django.bson.objectid import ObjectId
+#from bson.objectid import ObjectId
 
 logs_collection = get_db_handle()['logs']
 
 def log(object_type, id, message, details=None):
-    _id = str(ObjectId())
+    #_id = str(ObjectId())
     logs_collection.insert_one(
         {
-            "_id": _id, "timestamp": str(datetime.now()), "message": message, 
+            #"_id": _id, 
+            "timestamp": str(datetime.now()), "message": message, 
             "details" : details, "object":object_type, "object_id":id 
             }
         )
