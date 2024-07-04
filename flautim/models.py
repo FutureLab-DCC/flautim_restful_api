0a38b74f85de9caa8fb264a7e82afb188959953b
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2024-07-04 19:14:33 -0300

    Fixing ObjectID in _id bug at flautim/models.py

--- flautim/models.py
+++ flautim/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from utils import get_db_handle
 from datetime import datetime
+from django.bson.objectid import ObjectId
 
 logs_collection = get_db_handle()['logs']
 
 def log(object_type, id, message, details=None):
+    _id = str(ObjectId())
     logs_collection.insert_one(
-        {"timestamp": str(datetime.now()), "message": message, "details" : details,
-         "object":object_type, "object_id":id }
+        {
+            "_id": _id, "timestamp": str(datetime.now()), "message": message, 
+            "details" : details, "object":object_type, "object_id":id 
+            }
         )
