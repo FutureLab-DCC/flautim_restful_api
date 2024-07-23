2fc25f570152e7f5a8c84205d49765421ec985d8
Petrônio Cândido de Liam e Silva <petronio.candido@gmail.com>
2024-07-23 12:15:20 -0300

    Filesystem preprocessing for experiments

--- flautim/models.py
+++ flautim/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from utils import get_db_handle
 from datetime import datetime
-#from bson.objectid import ObjectId
+from pathlib import Path
+import shutil
 
 logs_collection = get_db_handle()['logs']
 
@@ -14,3 +15,55 @@ def log(object_type, id, message, details=None):
             "details" : details, "object":object_type, "object_id":id 
             }
         )
+    
+def get_experiment_files(base_path, id):
+    experiments = get_db_handle()['experimento']
+    projects = get_db_handle()['projeto']
+    models = get_db_handle()['modelo']
+    datasets = get_db_handle()['datasets']
+    attachments = get_db_handle()['Attachments']
+
+    experiment = experiments.find({"id": id})
+
+    project = projects.find({"id" : experiment["projectId"]})
+
+    base_folder = "{}/{}/{}/".format(base_path, project["sigla"], experiment["acronym"])
+
+    check_dir(base_folder)
+
+    for file_id in experiment["hyperparameterFile"]:
+        file = attachments.find({"id" : file_id})
+        copy_file(file["path"], "{}{}".format(base_folder, file["name"]))
+
+    for file_id in experiment["apiFile"]:
+        file = attachments.find({"id" : file_id})
+        copy_file(file["path"], "{}{}".format(base_folder, file["name"]))
+
+    model = models.find({"id" : experiment["modelId"]})
+
+    for file_id in model["archiveModel"]:
+        file = attachments.find({"id" : file_id})
+        copy_file(file["path"], "{}model/{}".format(base_folder, file["name"]))
+
+    dataset = datasets.find({"id" : experiment["datasetId"]})
+
+    for file_id in dataset["files"]:
+        file = attachments.find({"id" : file_id})
+        copy_file(file["path"], "{}data/{}".format(base_folder, file["name"]))
+
+
+    return base_folder
+
+
+
+
+def check_dir(path):
+    p = Path(path)
+    if not p.exists():
+        p.mkdir(parents=True)
+
+def copy_file(path_from, path_to):
+    pt = Path(path_to)
+    if not pt.exists():
+        pt.unlink()
+    shutil.copy(path_from, path_to)
