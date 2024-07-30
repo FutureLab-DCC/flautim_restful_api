8e89b63f9bc492c0557da99b3999abc7e7afb850
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2024-07-30 16:13:55 -0300

    Updating flautim-service.sh

--- flautim-service.sh
+++ flautim-service.sh
@@ -8,55 +8,29 @@
 # Description:       <DESCRIPTION>
 ### END INIT INFO
 
-HOME=
+HOME=/var/www/flautim
 
 DJANGO_COMMAND=$HOME/django.sh
 CELERY_COMMAND=$HOME/celery.sh
-RUNAS=root
-
-DJANGO_PIDFILE=/var/run/django.pid
-DJANGO_LOGFILE=/var/log/django.log
-
-CELERY_PIDFILE=/var/run/celery.pid
-CELERY_LOGFILE=/var/log/celery.log
 
 start() {
-  if [ -f $CELERY_PIDFILE ] && kill -0 $(cat $CELERY_PIDFILE); then
-    echo 'Service Celery is already running' >&2
-  else
-    echo 'Starting Celery service…' >&2
-    local CMD="$CELERY_COMMAND &> \"$CELERY_LOGFILE\" & echo \$!"
-    su -c "$CMD" $RUNAS > "$CELERY_PIDFILE"
-    echo 'Service Celery started' >&2
-  fi
-  
-  if [ -f $DJANGO_PIDFILE ] && kill -0 $(cat $DJANGO_PIDFILE); then
-    echo 'Service Django is already running' >&2
-  else
-    echo 'Starting Django service…' >&2
-    local CMD="$DJANGO_COMMAND &> \"$DJANGO_LOGFILE\" & echo \$!"
-    su -c "$CMD" $RUNAS > "$DJANGO_PIDFILE"
-    echo 'Service Django started' >&2
-  fi
+ nohup $CELERY_COMMAND & 
+ nohup $DJANGO_COMMAND &
 }
 
 stop() {
-  if [ ! -f "$DJANGO_PIDFILE" ] || ! kill -0 $(cat "$DJANGO_PIDFILE"); then
-    echo 'Service Django is not running' >&2
-  else 
-    echo 'Stopping Django service…' >&2
-    kill -15 $(cat "$DJANGO_PIDFILE") && rm -f "$DJANGO_PIDFILE"
-    echo 'Service Django stopped' >&2
-  fi
+  PIDS=`ps -ax | grep celery | awk -v OFS='\t' '{print $1 }'`
+  for PID in $PIDS
+  do
+    kill -9 $PID
+  done
 
-  if [ ! -f "$CELERY_PIDFILE" ] || ! kill -0 $(cat "$CELERY_PIDFILE"); then
-    echo 'Service Celery is not running' >&2
-  else 
-    echo 'Stopping Celery service…' >&2
-    kill -15 $(cat "$CELERY_PIDFILE") && rm -f "$CELERY_PIDFILE"
-    echo 'Service Celery stopped' >&2
-  fi
-  
+  PIDS=`ps -ax | grep manage.py | awk -v OFS='\t' '{print $1 }'`
+  for PID in $PIDS
+  do
+    kill -9 $PID
+  done
+
 }
 
 status() {
