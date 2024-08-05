8e29c144295fcd7994bf19c2bbd2c282d18f26e2
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2024-08-05 16:41:30 -0300

    Path adaptation to Web Interface attachments

--- utils.py
+++ utils.py
@@ -1,16 +1,22 @@
 from pymongo import MongoClient
 import yaml
 
-def get_db_handle():
+
+def read_config(name):
     with open('config.yaml') as f:
         try: 
-            config = yaml.safe_load(f)['mongodb']
-            client = MongoClient(host=config['host'],
-                    port=int(config['port']),
-                    username=config['username'],
-                    password=config['password']
-                     )
-            db_handle = client[config['db']]
-            return db_handle #, client
+            cfg = yaml.safe_load(f)[name]
+            return cfg
         except Exception as ex:
             raise ex
+
+
+def get_db_handle():
+    config = read_config('mongodb')
+    client = MongoClient(host=config['host'],
+                         port=int(config['port']),
+                         username=config['username'],
+                         password=config['password']
+                         )
+    db_handle = client[config['db']]
+    return db_handle
