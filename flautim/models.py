5c29562c15e36ae3e1540a9e1fe449b000a55836
Petrônio Cândido de Liam e Silva <petronio.candido@gmail.com>
2024-08-08 21:03:45 -0300

    Testing $$now mongodb operator for fixing timestamp issues in logs

--- flautim/models.py
+++ flautim/models.py
@@ -9,7 +9,7 @@ logs_collection = get_db_handle()['logs']
 def log(object_type, id, message, details=None):
     logs_collection.insert_one(
         {
-            "timestamp": str(datetime.now()), "message": message, 
+            "timestamp": "$$now", "message": message, 
             "details" : details, "object":object_type, "object_id":id 
             }
         )
